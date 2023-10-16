71f81e98878c236adee8dadb086bace8cb414a50
kauasilvaa <kaua60033@gmail.com>
2023-10-16 15:07:00 -0300

    teste de login

--- logout.php
+++ logout.php
@@ -1,11 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-<h1> TELA DE logout </h1>
-</body>
-</html>
+<?php
+session_start();
+session_destroy();
+header("location: login.php");
+exit;
+?>
